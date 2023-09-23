c842f46ea43f94cfbcd57238b39c79d7213ef3e9
Anthony Ancheta <anch767@gmail.com>
2023-09-23 13:18:45 +0800

    delete

--- scrimba/index.js
+++
@@ -1,9 +0,0 @@
-// When the user clicks the purchase button, render out
-// "Something went wrong, please try again" in the paragraph
-// that has the id="error".
-
-function alertPurchase(){
-    let alertText = "Something went wrong, please try again"
-    document.getElementById("error").textContent = "Something went wrong, please try again"
-    return alertText;        
-}
--- work project/work.js
+++
@@ -1,6 +0,0 @@
-const copyCaseJs = document.getElementById("copyCase");
-
-copyCaseJs.addEventListener("click", (event) => {
-    const content = document.getElementById("case").textContent;
-    navigator.clipboard.writeText(content);
-})
